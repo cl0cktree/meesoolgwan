21d26ab201dc6b70d7fb81920c3c4c4216fb5ade
Lee.Hyunsup <clocktree@live.co.kr>
2019-11-06 17:02:16 +0900

    20191106_1
    - upload.

---
+++ js/brand.js
@@ -0,0 +1,3 @@
+	$(function(){
+		$('#pop a').lightBox();
+	});
---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(".down").scroller_roll({
+                    title_show: 'disable',//enable  disable
+                    time_interval: '30',
+                    window_background_color: "none",
+                    border_size: '0',
+                    images_width: '306%',
+                    images_height: '306',
+                    images_margin: '0',
+                    title_size: '12',
+                    title_color: 'black',
+                    show_count: '9'
+     });				
+});
